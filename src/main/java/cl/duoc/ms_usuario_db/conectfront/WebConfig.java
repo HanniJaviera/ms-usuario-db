461069bafac2aaeffbc000b09ab7bdc01d9f5c0b
HanniJaviera <javiera.arc@hotmail.com>
2025-12-04 20:51:55 -0300

    arreglo de CORS para permitir solicitudes desde el front-end

--- src/main/java/cl/duoc/ms_usuario_db/conectfront/WebConfig.java
+++ src/main/java/cl/duoc/ms_usuario_db/conectfront/WebConfig.java
@@ -8,16 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Bean
+@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") 
-                        .allowedOriginPatterns("*") 
+                        .allowedOrigins("https://ms-usuario-db-production.up.railway.app/", "http://localhost:3000") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
